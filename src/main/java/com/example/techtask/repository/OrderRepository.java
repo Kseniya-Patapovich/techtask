2044036acb07a91960e81738c381b4923dbc5814
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-08-19 22:20:43 +0300

    Fixed teachtask

--- src/main/java/com/example/techtask/repository/OrderRepository.java
+++ src/main/java/com/example/techtask/repository/OrderRepository.java
@@ -8,12 +8,16 @@ import java.util.Optional;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-    @Query(nativeQuery = true, value = "SELECT * FROM orders " +
-            "WHERE quantity > 1 ORDER BY created_at DESC LIMIT 1;")
+    @Query(nativeQuery = true, value = """
+            SELECT * FROM orders
+            WHERE quantity > 1 ORDER BY created_at DESC LIMIT 1
+            """)
     Optional<Order> findNewestOrderWithMoreThanOneItem();
 
-    @Query(nativeQuery = true, value = "SELECT o.* FROM orders o JOIN users u ON o.user_id = u.id " +
-            "WHERE u.user_status = 'ACTIVE' " +
-            "ORDER BY o.created_at ASC;")
+    @Query(nativeQuery = true, value = """
+            SELECT o.* FROM orders o JOIN users u ON o.user_id = u.id
+            WHERE u.user_status = 'ACTIVE'
+            ORDER BY o.created_at ASC
+            """)
     List<Order> findOrdersFromActiveUsersOrderedByCreationDate();
 }
--- src/main/java/com/example/techtask/repository/UserRepository.java
+++ src/main/java/com/example/techtask/repository/UserRepository.java
@@ -8,17 +8,22 @@ import java.util.Optional;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query(nativeQuery = true, value = "SELECT u.* " +
-            "FROM users u " +
-            "JOIN orders o ON u.id = o.user_id " +
-            "WHERE EXTRACT(YEAR FROM o.created_at) = 2003 " +
-            "GROUP BY u.id " +
-            "ORDER BY SUM(o.price * o.quantity) DESC LIMIT 1;")
+    @Query(nativeQuery = true, value = """
+            SELECT u.* 
+            FROM users u
+            JOIN orders o ON u.id = o.user_id 
+            WHERE EXTRACT(YEAR FROM o.created_at) = 2003 
+            AND o.order_status = 'DELIVERED'
+            GROUP BY u.id 
+            ORDER BY SUM(o.price * o.quantity) DESC LIMIT 1
+            """)
     Optional<User> findUserWithMaxDeliveredItemsIn2003();
 
-    @Query(nativeQuery = true, value = "SELECT DISTINCT u.* " +
-            "FROM users u JOIN orders o ON u.id = o.user_id " +
-            "WHERE o.order_status = 'PAID' " +
-            "AND EXTRACT(YEAR FROM o.created_at) = 2010;")
+    @Query(nativeQuery = true, value = """
+            SELECT DISTINCT u.*
+            FROM users u JOIN orders o ON u.id = o.user_id
+            WHERE o.order_status = 'PAID'
+            AND EXTRACT(YEAR FROM o.created_at) = 2010
+            """)
     List<User> findUsersWithPaidOrdersIn2010();
 }
